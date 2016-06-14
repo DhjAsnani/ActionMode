ec9f4c3ca540bc1d1ae9cab65eb273184ca08d2c
dhjasnani <asnani.dheeraj@yahoo.com>
2016-06-14 15:32:50 +0530

    Card View Layout Created

---
+++ app/src/main/java/com/example/gohan/actionmode/Contact.java
@@ -0,0 +1,20 @@
+package com.example.gohan.actionmode;
+
+/**
+ * Created by Freeware Sys on 6/14/2016.
+ */
+public class Contact {
+    String name;
+
+    public Contact(String name)
+    {
+        this.setName(name);
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
