a5246b54425aecf2fd8bece8aa2045eca990ab43
dhjasnani <asnani.dheeraj@yahoo.com>
2016-06-14 17:05:40 +0530

    RecyclerView Created

--- app/src/main/java/com/example/gohan/actionmode/MainActivity.java
+++ app/src/main/java/com/example/gohan/actionmode/MainActivity.java
@@ -2,17 +2,40 @@ package com.example.gohan.actionmode;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+    RecyclerView.Adapter adapter;
     Toolbar toolbar;
+    ArrayList<Contact> arrayList = new ArrayList<Contact>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        // initialise recycler view
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+        String [] Name=getResources().getStringArray(R.array.names);
+        int i=0;
+        for(String NAME:Name)
+        {
+            Contact contact = new Contact(NAME);
+            arrayList.add(contact);
+            i++;
+        }
+        adapter = new RecyclerAdapter(arrayList,MainActivity.this);
+        recyclerView.setAdapter(adapter);
     }
     //adding menu to toolbar
 
---
+++ app/src/main/java/com/example/gohan/actionmode/RecyclerAdapter.java
@@ -0,0 +1,58 @@
+package com.example.gohan.actionmode;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Freeware Sys on 6/14/2016.
+ */
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.RecyclerViewHolder> {
+
+    ArrayList<Contact> adapter_list = new ArrayList<Contact>();
+    MainActivity mainActivity;
+    Context ctx;
+    public RecyclerAdapter(ArrayList<Contact> adapter_list,Context ctx){
+        this.adapter_list = adapter_list;
+        this.ctx = ctx;
+        mainActivity = (MainActivity) ctx;
+
+    }
+    @Override
+    public RecyclerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_view_layout,parent,false);
+        RecyclerViewHolder recyclerViewHolder= new RecyclerViewHolder(view,mainActivity);
+        return recyclerViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerViewHolder holder, int position) {
+        holder.textView.setText(adapter_list.get(position).getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return adapter_list.size();
+    }
+
+    public static class RecyclerViewHolder extends RecyclerView.ViewHolder{
+
+        TextView textView;
+        CheckBox checkBox;
+        MainActivity mainActivity;
+        public RecyclerViewHolder(View itemview, MainActivity mainActivity)
+        {
+            // mainActivity is used for handling the click events of the checkboxes
+            super(itemview);
+            textView = (TextView) itemview.findViewById(R.id.textView);
+            checkBox= (CheckBox) itemview.findViewById(R.id.checkBox);
+            this.mainActivity = mainActivity;
+        }
+    }
+}
