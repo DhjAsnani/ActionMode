e669fbed986f5982490e5ffaf4de411cb47bdf3f
dhjasnani <asnani.dheeraj@yahoo.com>
2016-06-14 15:00:29 +0530

    Menu files created, Menu icon image added

--- app/src/main/java/com/example/gohan/actionmode/MainActivity.java
+++ app/src/main/java/com/example/gohan/actionmode/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.gohan.actionmode;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
