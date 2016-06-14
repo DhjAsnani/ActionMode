cf920918f74d364749c2bb2c510ba13d7d50cc19
dhjasnani <asnani.dheeraj@yahoo.com>
2016-06-14 15:05:01 +0530

    Menu item added to toolbar, normal toolbar

--- app/src/main/java/com/example/gohan/actionmode/MainActivity.java
+++ app/src/main/java/com/example/gohan/actionmode/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.gohan.actionmode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
     Toolbar toolbar;
@@ -12,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
+    //adding menu to toolbar
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_activity_main,menu);
+        return true;
     }
 }
